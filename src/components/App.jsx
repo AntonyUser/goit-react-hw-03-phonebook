b71abc6352794f2ef01e7dc37b3b225b935be082
AntonyUser <101988185+AntonyUser@users.noreply.github.com>
2022-11-01 19:30:30 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
-import * as yup from 'yup';
+
 import { nanoid } from 'nanoid';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { ContactForm } from './ContactForm/ContactFomr';
-// import { Formik } from 'formik';
+import ContactForm from './ContactForm/ContactFomr';
+// import formik from 'formik';
 import { Container } from './App.styled';
-
-yup.addMethod(yup.string, 'validation', function () {
-  return this.matches(
-    "^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$",
-    "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  );
-});
-// yup.addMethod(yup.string, 'numeric', function () {
-//   return this.matches(
-//     '+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}',
-//     'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-//   );
-// });
-
-const Schema = yup.object().shape({
-  name: yup.string().validation().required(),
-  number: yup.string().required(),
-});
 
 class App extends Component {
   state = {
@@ -33,14 +15,16 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
     filter: '',
   };
 
   onSubmit = ({ name, number }) => {
-    const names = this.state.contacts.map(contact => contact.name);
-    if (!names.includes(name)) {
+    const isExist = this.state.contacts.find(contact => contact.name === name);
+    if (isExist && number) {
+      window.alert(`${name} is already in contacts`);
+      console.log(this.state.contacts);
+      return;
+    } else {
       const contact = {
         id: nanoid(),
         name,
@@ -49,19 +33,9 @@ class App extends Component {
       this.setState(prevState => {
         return {
           contacts: [contact, ...prevState.contacts],
-          name: '',
-          number: '',
         };
       });
-    } else if (!this.state.name) {
-      return;
-    } else {
-      window.alert(`${name} is already in contacts`);
     }
-  };
-  onChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
   };
 
   onChangeFilter = e => {
@@ -84,13 +58,7 @@ class App extends Component {
     return (
       <Container>
         <h2>Phonebook</h2>
-        <ContactForm
-          schema={Schema}
-          onSubmit={this.onSubmit}
-          name={this.state.name}
-          number={this.state.number}
-          onChange={this.onChange}
-        />
+        <ContactForm onSubmit={this.onSubmit} />
         <h2>Contacts</h2>
         <Filter value={this.state.filter} onChange={this.onChangeFilter} />
         <ContactList
